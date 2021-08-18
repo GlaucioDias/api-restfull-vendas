72468155536c17feb6cba582e1cca89e35443bdd
GlaucioDias <fleevrl@gmail.com>
2021-08-18 17:40:05 -0300

    implementando token jwt

---
+++ src/modules/users/routes/sessions.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+import SessionsController from '../constrollers/SessionsController';
+
+const sessionsRouter = Router();
+const sessionsController = new SessionsController();
+
+sessionsRouter.route('/').post(
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  sessionsController.create,
+);
+
+export default sessionsRouter;
