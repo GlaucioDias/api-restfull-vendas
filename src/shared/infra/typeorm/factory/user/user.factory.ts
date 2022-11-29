ee9a3f4ca9525607661b6739b3005552e750fdb0
GlaucioDias <fleevrl@gmail.com>
2022-11-28 23:25:26 -0300

    factory gerando usuário com password encripado

--- src/shared/infra/typeorm/factory/user/user.factory.ts
+++ src/shared/infra/typeorm/factory/user/user.factory.ts
@@ -1,18 +1,22 @@
 import { faker } from '@faker-js/faker';
 import { define } from 'typeorm-seeding';
 import User from '@modules/users/infra/typeorm/entities/User';
+import { hashSync } from 'bcryptjs';
 
 const users: User[] = new Array<User>();
 
 define(User, () => {
   const user = new User();
+  const salt = 8
+  const password = '123456'
 
   user.id = faker.datatype.uuid();
   user.name = faker.name.firstName();
   user.email = faker.internet.email().toLocaleLowerCase();
-  user.password = faker.internet.password();
+  user.password = hashSync(password, salt);
 
   users.push(user);
 
   return user;
 });
+
