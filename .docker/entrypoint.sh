143d2866d9a61a72f2bb92ded1af8f70f1a3c5ae
GlaucioDias <fleevrl@gmail.com>
2022-11-28 23:16:58 -0300

    Ajustes: - Arquivo .env de exemplo - Arquivo ormconfig de exemplo - Adequa arquivo de shell script

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+if [ ! -f ".env" ]; then
+  cp .env.example .env
+fi
+
+if [ ! -f "ormconfig.json" ]; then
+  cp ormconfig.example ormconfig.json
+fi
+
 yarn install
-DROP SCHEMA public CASCADE
-CREATE SCHEMA public
+
+yarn run typeorm -- -d src/shared/infra/typeorm/index.ts schema:drop
 
 yarn run typeorm -- -d src/shared/infra/typeorm/index.ts migration:run
-sleep 5s
+
 yarn seed:run
 
 yarn run dev
